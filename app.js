240a237b177118c2bd5708159f33d32594bf9165
Jayk5 <jayantkumar20@cse.iiitp.ac.in>
2021-08-02 20:49:13 +0530

    Logic Built, TODO- drop down

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ let resetB = document.querySelector('#reset');
 let p1score = 0;
 let p2score = 0;
 let winningScore = 3;
+let state = false;
+
 p1Button.addEventListener('click', () => {
+    checkB1();
     if (p1score < winningScore) {
         p1score++;
         p1Display.innerText = p1score;
     }
 })
 p2Button.addEventListener('click', () => {
+    checkB2();
     if (p2score < winningScore) {
         p2score++;
         p2Display.innerText = p2score;
@@ -21,6 +25,27 @@ p2Button.addEventListener('click', () => {
 resetB.addEventListener('click', () => {
     p1Display.innerText = 0;
     p2Display.innerText = 0;
+    p1Button.disabled = false;
+    p2Button.disabled = false;
+    p1Display.style.color = 'black';
+    p2Display.style.color = 'black';
     p1score = 0;
     p2score = 0;
 })
+function checkB1() {
+    if (p1score === winningScore - 1) {
+        state = true;
+        p1Display.style.color = 'green';
+        p2Display.style.color = 'red';
+        p1Button.disabled = true;
+    }
+}
+function checkB2() {
+    if (p2score === winningScore - 1) {
+        state = true;
+        p1Display.style.color = 'red';
+        p2Display.style.color = 'green'
+        p2Display.classList.add('win');
+        p2Button.disabled = true;
+    }
+}
