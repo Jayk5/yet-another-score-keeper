579a1a85691f1a9a9e6b004558271d0fe50c1824
Jayk5 <jayantkumar20@cse.iiitp.ac.in>
2021-08-03 20:49:43 +0530

    final build

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-let p1Display = document.querySelector('#p1s');
-let p2Display = document.querySelector('#p2s');
-let p1Button = document.querySelector('#p1b');
-let p2Button = document.querySelector('#p2b');
+// let p1Display = document.querySelector('#p1s');
+// let p2Display = document.querySelector('#p2s');
+// let p1Button = document.querySelector('#p1b');
+// let p2Button = document.querySelector('#p2b');
 let resetB = document.querySelector('#reset');
 let scoreSelect = document.querySelector('#ScoreSelect')
-let p1score = 0;
-let p2score = 0;
+// let p1score = 0;
+// let p2score = 0;
 let winningScore = 3;
 let state = false;
+
+p1 = {
+    score: 0,
+    display: document.querySelector('#p1s'),
+    button: document.querySelector('#p1b'),
+}
+p2 = {
+    score: 0,
+    display: document.querySelector('#p2s'),
+    button: document.querySelector('#p2b'),
+}
+
+function clickB(player, opponent) {
+    if (state === false) {
+        player.score++;
+        if (player.score === winningScore) {
+            state = true;
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+        }
+        player.display.innerText = player.score;
+    }
+}
 
 scoreSelect.addEventListener('change', function () {
     winningScore = parseInt(this.value);
@@ -15,41 +40,21 @@ scoreSelect.addEventListener('change', function () {
 })
 const reset = () => {
     state = false;
-    p1Display.innerText = 0;
-    p2Display.innerText = 0;
-    p1Button.disabled = false;
-    p2Button.disabled = false;
-    p1Display.classList.remove('has-text-success', 'has-text-danger');
-    p2Display.classList.remove('has-text-success', 'has-text-danger');
-    p1score = 0;
-    p2score = 0;
+    p1.display.innerText = 0;
+    p2.display.innerText = 0;
+    p1.button.disabled = false;
+    p2.button.disabled = false;
+    p1.display.classList.remove('has-text-success', 'has-text-danger');
+    p2.display.classList.remove('has-text-success', 'has-text-danger');
+    p1.score = 0;
+    p2.score = 0;
 }
-p1Button.addEventListener('click', () => {
-    if (state === false) {
-        p1score++;
-        if (p1score === winningScore) {
-            state = true;
-            p1Button.disabled = true;
-            p2Button.disabled = true;
-            p1Display.classList.add('has-text-success');
-            p2Display.classList.add('has-text-danger');
-        }
-        p1Display.innerText = p1score;
-    }
-})
-p2Button.addEventListener('click', () => {
-    if (state === false) {
-        p2score++;
-        if (p2score === winningScore) {
-            state = true;
-            p1Button.disabled = true;
-            p2Button.disabled = true;
-            p2Display.classList.add('has-text-success');
-            p1Display.classList.add('has-text-danger');
-        }
-        p2Display.innerText = p2score;
-    }
-})
+p1.button.addEventListener('click', () => {
+    clickB(p1, p2);
+});
+p2.button.addEventListener('click', () => {
+    clickB(p2, p1);
+});
 resetB.addEventListener('click', reset)
 
 // function checkB1() {
